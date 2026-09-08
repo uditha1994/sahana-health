46a8c808f604082994922b26e74ab54297c62ac2
uditha1994 <uditha@programmer.net>
2026-09-08 11:27:53 +0530

    patient routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,6 +1,11 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\PatientController;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+
+$routes -> get('patients', 'PatientController::index');
+$routes -> get('/patient/create', 'PatientController::create');
+$routes -> post('patient/store', 'PatientController::store')
